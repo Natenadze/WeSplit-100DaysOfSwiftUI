c2f33bd91a7984fa0c10cb3498ea68252670ebb7
davit <davit.natenadze@gmail.com>
2023-05-29 19:10:54 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Davit Natenadze on 29.05.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
